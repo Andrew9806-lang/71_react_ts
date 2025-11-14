f35884a2c20ebfbc7d86c4cffa9b6e783d8afdf9
Andrew9806-lang <kornilovaamina20@gmail.com>
2025-11-14 15:03:55 +0300

    Homework12

---
+++ src/Homework/Homework12/Homework.tsx
@@ -0,0 +1,14 @@
+import EmployeeForm from "../../Components/EmployeeForm/EmployeeForm";
+import { HomeWorkWrapper } from "./style";
+
+
+
+
+function Homework12() {
+return(
+    <HomeWorkWrapper>
+        <EmployeeForm />
+    </HomeWorkWrapper>
+)
+}
+export default Homework12;
